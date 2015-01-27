bb960a01ee6d23c60f9767091ac3fa188016ad73
Sun Ning <sunng@about.me>
2015-01-27 14:06:05 +0800

    (chore) added url and license in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject cn.leancloud/clj-msgpack "0.4.1-SNAPSHOT"
+  :url "https://github.com/leancloud/clj-msgpack"
+  :license {:name "MIT"}
   :description "Messagepack serialization library for Clojure. LeanCloud fork."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.msgpack/msgpack "0.6.11" :exclusions [junit]]]
