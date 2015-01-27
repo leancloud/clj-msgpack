5c38f738f160a9bbbff1e36cbc68d887251e8635
Sun Ning <sunng@about.me>
2015-01-27 14:01:33 +0800

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cn.leancloud/clj-msgpack "0.4.0"
+(defproject cn.leancloud/clj-msgpack "0.4.1-SNAPSHOT"
   :description "Messagepack serialization library for Clojure. LeanCloud fork."
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.msgpack/msgpack "0.6.11" :exclusions [junit]]]
