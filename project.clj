15a984c2343572063df22969bf2c87ac0a01009a
Sun Ning <sunng@about.me>
2015-03-02 16:05:00 +0800

    (chore) update msgpack-java

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :license {:name "MIT"}
   :description "Messagepack serialization library for Clojure. LeanCloud fork."
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.msgpack/msgpack-core "0.7.0-p5" :exclusions [junit]]]
+                 [org.msgpack/msgpack-core "0.7.0-p7" :exclusions [junit]]]
   :deploy-repositories {"releases" :clojars}
   :jvm-opts ["-Xmx256m"])
